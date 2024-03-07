586c8b6cb932eabef40faa9fcba98ef6324b8d93
axel tavares <tavaresaxel95@gmail.com>
2024-03-06 23:17:14 -0500

    saving progress

--- server.js
+++ server.js
@@ -8,23 +8,38 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extendedn:true}))
+const fs = require("fs")
 
 
 
 //API route
 
+app.get("/api/notes", (req,res) => {
+    fs.readFile("./Assets/db/db.json", "utf-8", (err, data) =>{
+        const newData =JSON.parse(data)
+        res.json(newData)
+    })
+})
 
 
-
+app.post("/api/notes", (req,res) => {
+    
+})
 
 //htmal route
-app.get("*" , (req,res) =>{
+app.get("*", (req,res) =>{
+    res.sendFile(path.join(_dirname, './public/index.html'))
+})
 
+app.get("/notes", (req,res) =>{
+    fs.readFile("./Assets/db/db.json", "utf-8", (err, data) =>{
+        const db = JSON.parse
+    })
 })
 
 
 
 
 app.listen(PORT, ()=>{
-    console.log("App is listening at PORT: http://localhost:3001/"+PORT)
+    console.log("App is listening at PORT: http://localhost"+PORT)
 })
